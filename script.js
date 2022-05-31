eb0865f37b1f12547c11ffa3ffb41027b53d41eb
Tushar <tusharfaliya8@gmail.com>
2022-05-31 11:14:20 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,37 @@
+let navbar = document.querySelector('.navbar')
+
+
+fasttime.addEventListener('click',()=>{
+    cartjk.classList.remove('cart-item')
+          
+})
+
+
+document.querySelector('#menu-btn').onclick = ()=>{
+    navbar.classList.toggle('active')
+    cartitem.classList.remove('active')
+    searchform.classList.remove('active')
+}
+
+let cartitem = document.querySelector('.cart-item-container')
+
+document.querySelector('#cart-btn').onclick = ()=>{
+    cartitem.classList.toggle('active')
+    navbar.classList.remove('active')
+    searchform.classList.remove('active')
+}
+
+let searchform = document.querySelector('.serach-form')
+
+document.querySelector('#search-btn').onclick = ()=>{
+    searchform.classList.toggle('active')
+    navbar.classList.remove('active')
+    cartitem.classList.remove('active')
+}
+    
+
+window.onscroll=()=>{
+       navbar.classList.remove('active')
+       cartitem.classList.remove('active')
+       searchform.classList.remove('active')
+}
